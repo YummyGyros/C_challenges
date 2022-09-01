25397ad7cc21b5a22fae970c453c771e012180bb
Robin Levavasseur <robin.levavasseur@epitech.eu>
2022-09-01 11:12:51 +0200

    feat: gitignore feat: learning ncurses again: windows and borders

--- snake/main.c
+++ snake/main.c
@@ -1,4 +1,17 @@
-int main(int ac, char **av)
+#include <ncurses.h>
+
+int main()
 {
+    initscr();
+    WINDOW *win = newwin(15, 17, 2, 10);
+    refresh();
+
+    box(win, 0, 0);
+    mvwprintw(win, 1, 1, "Hello,");
+    mvwprintw(win, 2, 1, "World!");
+    wrefresh(win);
+
+    getch();
+    endwin();
     return 0;
 }
